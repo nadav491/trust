11c3c318c37b6fa3a77c425a98eaf100c9a84235
barmerhav <barmerhav@gmail.com>
2019-12-16 17:57:55 +0200

    oh shit

--- app/src/main/java/com/example/trust/MapsActivity.java
+++ app/src/main/java/com/example/trust/MapsActivity.java
@@ -11,10 +11,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
